956df67b7a2f8f293cabd42ed270f91e45907130
iamcharlie17 <riyadhosen40@gmail.com>
2025-08-31 15:18:29 +0600

    replace zustand to context api

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -1,8 +1,52 @@
+'use client'
+import { createContext, useContext, useState, useEffect } from "react";
 
-const { create } = require("zustand");
+export const AuthContext = createContext();
 
-export const authStore = create((set) => ({
-  user: null,
-  setUser: (user) => set({ user }),
-}));
 
+export const useAuth = () => {
+  return useContext(AuthContext);
+};
+
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const token = localStorage.getItem("token"); 
+    if (!token) return;
+
+    const fetchUser = async () => {
+      try {
+        const res = await fetch("http://localhost:3001/api/auth/get-user", {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+          cache: "no-store",
+        });
+
+        if (res.ok) {
+          const data = await res.json();
+          setUser(data);
+        } else {
+          setUser(null);
+        }
+      } catch (err) {
+        console.error("Error fetching user:", err);
+        setUser(null);
+      }
+    };
+
+    fetchUser();
+  }, [setUser]);
+  const logout = () => {
+    setUser(null);
+    localStorage.removeItem("token");
+  };
+
+  return (
+    <AuthContext.Provider value={{ user, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
